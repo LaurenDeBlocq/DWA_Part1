712766d18279a84528b9ac3014ab6fb094047499
Lauren de Blocq <lauren.deblocq@gmail.com>
2023-08-31 12:51:52 +0200

    redid DWA05

--- DWA05/scripts.js
+++ DWA05/scripts.js
@@ -5,5 +5,21 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  
+  try {
+    if (!dividend || !divider){
+      result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    } else if(dividend < 0 || divider <= 0){
+      result.innerText = "Division not performed. Invalid number provided. Try again";
+      throw new Error();
+    } else if (isNaN(dividend) || isNaN(divider)){
+      document.body.innerHTML = `
+      <h1>Something critical went wrong. Please reload the page.</h1>`;
+      throw new Error();
+    } else {
+    result.innerText = Math.floor(dividend / divider);
+    } 
+  } catch (e) {
+    console.trace()
+  }
 });
